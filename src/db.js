8fcdbbc31f2247dc806d3c1a199d58d87b8a6dd2
chrisruigz-ux <chris.ruigz@gmail.com>
2026-06-28 19:05:43 +0800

    Add src/db.js

---
+++ src/db.js
@@ -0,0 +1,30 @@
+﻿const fs = require("fs");
+const path = require("path");
+
+const DB_PATH = process.env.DB_PATH || "./data/worldcup.json";
+
+function getDbPath() {
+  return path.resolve(DB_PATH);
+}
+
+function ensureDir() {
+  const dir = path.dirname(getDbPath());
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+}
+
+function readDb() {
+  ensureDir();
+  if (!fs.existsSync(getDbPath())) {
+    const initial = { users: [], payment_orders: [], news_cache: [], match_schedule: [] };
+    fs.writeFileSync(getDbPath(), JSON.stringify(initial));
+    return initial;
+  }
+  return JSON.parse(fs.readFileSync(getDbPath(), "utf-8"));
+}
+
+function writeDb(data) {
+  ensureDir();
+  fs.writeFileSync(getDbPath(), JSON.stringify(data, null, 2));
+}
+
+module.exports = { readDb, writeDb };
